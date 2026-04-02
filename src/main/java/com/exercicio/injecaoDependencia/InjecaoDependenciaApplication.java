45bf6d25ce15322f4779be5c72d612fd78a41275
Allan <allanpereira773@gmail.com>
2026-04-02 16:21:33 -0300

    Project Created

---
+++ src/main/java/com/exercicio/injecaoDependencia/InjecaoDependenciaApplication.java
@@ -0,0 +1,20 @@
+package com.exercicio.injecaoDependencia;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class InjecaoDependenciaApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjecaoDependenciaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
